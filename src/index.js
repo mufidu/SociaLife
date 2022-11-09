4958d829a2e5bee2a0b2ff1fbdf3fb1368236a99
naufalabdillah1908 <84857455+naufalabdillah1908@users.noreply.github.com>
2022-11-09 12:40:20 +0700

    UI register-login

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-    <App />,
-    document.getElementById("root")
+  <App/>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
