72e039386a2208fe0079a0cffa545a4f4528bc9e
mufidu <mufid.to@gmail.com>
2022-11-10 14:00:42 +0700

    Update webpage title

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Home from "./pages/Home/Home";
 function App() {
   return (
     <div className="App">
-        <div className="blur" style={{top: '-18%', right: '0'}}></div>
-        <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
-        {<Home/>}
-        {/* {<Profile/>} */}
-        {/* {<Auth/>} */}
+      <div className="blur" style={{ top: '-18%', right: '0' }}></div>
+      <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
+      {/* {<Home/>} */}
+      {/* {<Profile/>} */}
+      {<Auth />}
     </div>
   );
 }
