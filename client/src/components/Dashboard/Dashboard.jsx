ed9720d89d33aceeeb12c776facb70ecdd07e8a6
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2022-11-11 00:10:33 +0700

    add logout

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import "./Dashboard.css";
 import small_logo from "../../img/small_logo.png";
 import person_logo from "../../img/person-fill.svg";
+import { useDispatch } from "react-redux";
+import { logOut } from "../../actions/AuthAction";
 
 const Dashboard = () => {
+  const dispatch = useDispatch()
+  const handleLogout = () => {
+    dispatch(logOut())
+  }
   return (
     <div className="Dashboard">
       <div style={{ marginLeft: "5px", marginTop: "10px" }}>
@@ -20,6 +26,10 @@ const Dashboard = () => {
       <div style={{ marginTop: "40rem", marginLeft: "5px" }}>
         <img src={person_logo} alt="" width="32px" height="32px" />
       </div>
+
+      <button className="button logout-button" style={{ cursor:'pointer' }} onClick={handleLogout}>
+        Log Out
+      </button>
     </div>
   );
 };
