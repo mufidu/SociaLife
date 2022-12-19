25765aa0c080b025c6e78dec48326021f5e831f7
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2022-12-20 02:37:09 +0700

    coba setTimeout buat alert

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,16 @@ import Auth from "./pages/Auth/Auth";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Chat from "./pages/Chat/Chat";
+import { useEffect } from "react";
 
 function App() {
   const user = useSelector((state) => state.authReducer.authData);
+
+  useEffect(() => {
+    const timer = setTimeout(() => alert('cukup bro, mata lu kasian. coba pandang objek 20 meter selama 20 detik dulu ya'), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+  
   return (
     <div className="App">
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
